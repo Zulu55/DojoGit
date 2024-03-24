8b8f0d22c9a453bc575a7b753e2a7c4f8bd130dd
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-24 16:01:30 -0500

    Complete classes

---
+++ Employee.cs
@@ -0,0 +1,7 @@
+public class Employee
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public decimal Salary { get; set; }
+}
